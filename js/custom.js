ee632537499bc27607a5f5f317b2a6f3aa25eaa0
francoisboulou <giscardboulou@gmail.com>
2019-06-30 13:58:00 -0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,160 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+4. Init Home Slider
+5. Init Gallery
+6. Init Testimonials Slider
+7. Init Lightbox
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/*
+
+	1. Vars and Inits
+
+	*/
+
+	let header = document.getElementById('header');
+	let menuActive = false;
+	let menu = document.getElementById('menu');
+	let burger = document.getElementById('burger_container');
+	let logo = document.getElementById('nav_logo_img');
+
+
+	/*
+	2. Set Header
+	*/
+
+	function setHeader()
+	{
+
+		if ((document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) && ($(window).width() < 991)) {
+			header.classList.add("scrolled");
+			logo.classList.add("logo-scrolled");
+			header.style.backgroundColor = 'rgba(255,255,255,.8)';
+		}
+		else if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100)
+		{
+			header.classList.add("scrolled");
+			logo.classList.add("logo-scrolled");
+		}
+		else if ((document.body.scrollTop < 100 || document.documentElement.scrollTop < 100) && ($(window).width() < 991))
+		{
+			header.classList.remove("scrolled");
+			logo.classList.remove("logo-scrolled");
+			header.style.backgroundColor = 'rgba(255,255,255,0)';
+		}
+		else if ((document.body.scrollTop < 100 || document.documentElement.scrollTop < 100) && ($(window).width() > 991))
+		{
+			header.classList.remove("scrolled");
+			logo.classList.remove("logo-scrolled");
+			header.style.backgroundColor = 'rgba(255,255,255,.5)';
+		}
+		// else
+		// {
+		// 	header.classList.remove("scrolled");
+		// 	logo.classList.remove("logo-scrolled");
+		// }
+	}
+
+	setHeader();
+
+	window.onresize = function()
+	{
+		setHeader();
+	}
+
+	window.onscroll = function()
+	{
+		setHeader();
+	}
+
+
+	/*
+	3. Init Menu
+	*/
+
+	function openMenu()
+	{
+		menu.classList.add("active");
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		menu.classList.remove("active");
+		menuActive = false;
+	}
+
+	function initMenu()
+	{
+		if(menu)
+		{
+			if(burger_container)
+			{
+				burger.onclick = function()
+				{
+					if(menuActive)
+					{
+						closeMenu();
+					}
+					else
+					{
+						openMenu();
+
+						$(document).one('click', function cls(e)
+						{
+							if($(e.target).hasClass('menu_mm'))
+							{
+								$(document).one('click', cls);
+							}
+							else
+							{
+								closeMenu();
+							}
+						})
+					}
+				}
+			}
+		}
+	}
+
+
+
+
+	/*
+	6. Init Testimonials Slider
+	*/
+
+	function initTestSlider()
+	{
+		if($('.test_slider').length)
+		{
+			var testSlider = $('.test_slider');
+			testSlider.owlCarousel(
+			{
+				items:1,
+				loop:true,
+				autoplay:true,
+				smartSpeed:1200,
+				nav:false,
+				dots:false
+			});
+		}
+	}
+
+	initMenu();
+	initTestSlider();
+
+});
