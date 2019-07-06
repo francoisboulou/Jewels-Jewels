f3446cd75da9e9c6f1e4a89d1daae1a4caeefcf6
francoisboulou <giscardboulou@gmail.com>
2019-07-05 22:45:38 -0600

    updated php file

--- index.php
+++ index.php
@@ -24,6 +24,8 @@
 				<li><a href="about.php">about</a></li>
 				<li><a href="https://www.etsy.com/shop/rubysvintagejewelry?ref=condensed_trust_header_title_items" target="_blank">store</a></li>
 				<li><a href="contact.php">contact</a></li>
+				<li><a href=""><img class='search_icon' src="./images/shopping_cart.png" alt=""></a></li>
+				<li><a href=""><img class='search_icon' src="./images/search.png" alt=""></a></li>  
 			</ul>
 		</nav>
 		<div id="burger_container" class="menu_mm"><div></div><div></div><div></div></div>
@@ -53,124 +55,100 @@
 
 			<section id="home_container">
 				<div>
-						<h1>Spring Sale</h1>
-						<h2>20% off Diamond Fashions</h2>
+						<h1>SPRING SALE</h1>
+						<h2>20% OFF DIAMOND STYLES</h2>
 						<a class="button" href="https://www.etsy.com/shop/rubysvintagejewelry?ref=condensed_trust_header_title_items">Shop Now</a>
 				</div>
 			</section>
 
 			<!-- Collections -->
-			<div id= "margin">
+			
 
-				<section id="collections">
-					<h1 class="section_title">Our Collections</h1>
-					<!-- Collections Item -->
-					<a class="collections_image" id="collections_image_1" href="https://www.etsy.com/shop/rubysvintagejewelry?ref=simple-shop-header-name&listing_id=545886983&section_id=15959220">Engagement Rings</a>
-					<!-- Collection Item -->
-					<a class="collections_image" id="collections_image_2" href="https://www.etsy.com/shop/rubysvintagejewelry?ref=simple-shop-header-name&listing_id=545886983&section_id=15959222">Men's Collection</a>
-					<!-- Collection Item -->
-					<a class="collections_image" id="collections_image_3" href="https://www.etsy.com/shop/rubysvintagejewelry?ref=simple-shop-header-name&listing_id=545886983&section_id=16036813">Unique Vintage</a>
-				</section>
+			<section id='collections'>
+				<!-- <h2 class="section_title">Our Collections</h2> -->
+					<a class='collections_image' id="collections_image_1" href="https://www.etsy.com/shop/rubysvintagejewelry?ref=simple-shop-header-name&listing_id=545886983&section_id=15959220"><div class="overlay"><p>Collection One</p></div>
+					</a>			
+					<div id='collections_right'>
+						<a class='collections_image' id="collections_image_2" href="https://www.etsy.com/shop/rubysvintagejewelry?ref=simple-shop-header-name&listing_id=545886983&section_id=15959222"><div class="overlay"><p>Collection Two</p></div></a>
+				
+						<a class='collections_image' id="collections_image_3" href="https://www.etsy.com/shop/rubysvintagejewelry?ref=simple-shop-header-name&listing_id=545886983&section_id=16036813"><div class="overlay"><p>Collection Three</p></div></a>
+					</div>
+			</section>
 
 
 				<!-- New Arrivals -->
 
-				<section id="arrivals">
-					<h1 class="section_title">new arrivals</h1>
+			<section id="arrivals">
+				<!-- <h1 class="section_title">new arrivals</h1> -->
 
-					<!-- Product -->
-					<div class="product">
-						<img class="product_image" src="images/product_3.jpg" alt="">
-						<div class="product_info">
-							<a class="product_name" href="">Engagement Ring</a>
-							<p class="product_price">$145.00</p>
-						</div>
+				<!-- Product -->
+				<a class="product" href="#">
+					<div id='product_1' class="product_image"></div>
+					<div class="product_info product_name">
+						<span>Sunstone Ring</span> 
+						<p class="product_price">$145.00</p>
 					</div>
+				</a>
 
-					<!-- Product -->
-					<div class="product">
-						<img class="product_image" src="images/product_1.jpg" alt="">
-						<div class="product_info">
-							<a class="product_name" href="https://www.etsy.com/listing/682051286/vintage-victorian-18k-white-gold?ref=shop_home_active_24&frs=1"><span>Victorian</span> <span>White Gold</span> <span>Diamond Ring</span></a>
-							<p class="product_price">$1,350.00</p>
-						</div>
+				<!-- Product -->
+				<a class="product" href="https://www.etsy.com/listing/682051286/vintage-victorian-18k-white-gold?ref=shop_home_active_24&frs=1">
+					<div id='product_2' class="product_image"></div>
+					<div class="product_info product_name">
+						<span>Victorian</span> <span>White Gold</span> <span>Diamond Ring</span>
+						<p class="product_price">$1,350.00</p>
 					</div>
+				</a>
 
-					<!-- Product -->
-					<div class="product">
-						<img class="product_image" src="images/product_2.jpg" alt="">
-						<div class="product_info">
-							<a class="product_name" href="https://www.etsy.com/listing/682056282/substantial-natural-star-sapphire-and?ref=shop_home_active_23&frs=1"><span>White Gold</span> <span>Star Saphire Ring</span></a>
-							<p class="product_price">$1,285.00</p>
-						</div>
-					</div>
-				</section>
+				<!-- Product -->
+				<a class="product" href="https://www.etsy.com/listing/682056282/substantial-natural-star-sapphire-and?ref=shop_home_active_23&frs=1">
+					<div id='product_3' class="product_image"></div>
+					<div class="product_info product_name">
+						<span>White Gold</span> <span>Star Saphire Ring</span>
+						<p class="product_price">$1,285.00</p>
+					</div>					
+				</a>
+			</section>
 
 
 				<!-- Extra -->
-				<section id="extra">
-			  	<section class="extra_promo" id="extra_promo_1" style="background-image:url(images/extra_1.jpg)">
-						<div class="extra_content" id="extra_1">
-							<p id="extra_1_price">20%<span> off</span></p>
-							<p id="extra_1_title">All Diamond Jewelry</p>
-							<p id="extra_1_text">Limited time offer. Offer may not be combined with any other sale, promotion, discount, code, or coupon.</p>
-							<a class="button" href="checkout.php">shop now</a>
-						</div>
-					</section>
-
-					<section class="extra_promo" id="extra_promo_2">
-						<div class="extra_content" id="extra_2">
-							<div id="extra_2_text">Native<br>American<br>Collection</div>
-							<a class="button" href="checkout.php">shop now</a>
-						</div>
-					</section>
-				</section>
+			<section id="extra">
+				<div id="extra_text">Native American Collection</div>
+				<a class="button" href="checkout.php">shop now</a>
+			</section>
 
 				<!-- Testimonials -->
 
-				<section id="testimonials">
-					<h1 class="section_title">testimonials</h1>
+			<section id="testimonials">
+				<!-- <h1 class="section_title">testimonials</h1> -->
 
-					<!-- Testimonials Slider -->
-					<div class="owl-carousel owl-theme test_slider">
+				<!-- Testimonials Slider -->
+				<div class="owl-carousel owl-theme test_slider">
 
-						<!-- Testimonial Item -->
-						<div class="owl-item">
-							<div class="test_text">“Outstanding customer service. When I inquired about this ring, they answered all my questions and went above and beyond to make sure I was happy with my decision. I ordered on a Friday and it was in my mailbox Monday. I highly recommend this seller.”</div>
-							<div class="test_name">Dominique H.</div>
-						</div>
-
-						<!-- Testimonial Item -->
-						<div class="owl-item">
-							<div class="test_text">“A wonderful buying experience. My rare, unique item arrived ahead of schedule, packaged with care, and looked exactly as described. Communication with them was amazing, they were always available when I had a question. I'll definitely buy from Ruby's Vintage again.”</div>
-							<div class="test_name">Scott W.</div>
-						</div>
-
-						<!-- Testimonial Item -->
-						<div class="owl-item">
-							<div class="test_text">“I have purchased many pieces from Ruby's Vintage over the years and each is a truly unique treasure. I am not easily impressed, but the fast shipping, and excellent communication of Ruby's Vintage have made me a regular customer. I highly recommend them”</div>
-							<div class="test_name">Sandra G.</div>
-						</div>
-
+					<!-- Testimonial Item -->
+					<div class="owl-item">
+						<div class="test_text">“Outstanding customer service. When I inquired about this ring, they answered all my questions and went above and beyond to make sure I was happy with my decision. I ordered on a Friday and it was in my mailbox Monday. I highly recommend this seller.”</div>
+						<div class="test_name">Dominique H.</div>
 					</div>
-				</section>
 
-				<!-- Newsletter -->
-
-				<section class="newsletter newsletter_image" style="background-image:url(images/newsletter.jpg)">
-					<div class="newsletter_content">
-						<h1>subscribe for a 20% discount</h1>
-						<form action="#">
-							<input type="email" class="newsletter_input" required="required" placeholder="E-mail here">
-							<button type="submit" class="button">subscribe</button>
-						</form>
-						<p class="newsletter_text">One time offer. Offer may not be combined with any other sale, promotion, discount, code, or coupon.</p>
+					<!-- Testimonial Item -->
+					<div class="owl-item">
+						<div class="test_text">“A wonderful buying experience. My rare, unique item arrived ahead of schedule, packaged with care, and looked exactly as described. Communication with them was amazing, they were always available when I had a question. I'll definitely buy from Ruby's Vintage again.”</div>
+						<div class="test_name">Scott W.</div>
 					</div>
-				</section>
-				<section>
-					<div id='instafeed'></div>
-				</section>
-			</div>
+
+					<!-- Testimonial Item -->
+					<div class="owl-item">
+						<div class="test_text">“I have purchased many pieces from Ruby's Vintage over the years and each is a truly unique treasure. I am not easily impressed, but the fast shipping, and excellent communication of Ruby's Vintage have made me a regular customer. I highly recommend them”</div>
+						<div class="test_name">Sandra G.</div>
+					</div>
+
+				</div>
+			</section>	
+			<section id='instaCont'>
+				<div id='instafeed'></div>
+			</section>
+			
+			
 		</article>
 	</main>
 
@@ -179,26 +157,36 @@
 	<!-- Footer -->
 
 	<footer>
-		<a href="#">Ruby's Vintage Jewelry</a>
-		<nav>
-			<ul>
-				<li><a href="#">home</a></li>
-				<li><a href="about.php">about</a></li>
-				<li><a href="https://www.etsy.com/shop/rubysvintagejewelry?ref=condensed_trust_header_title_items">store</a></li>
-				<li><a href="contact.php">contact</a></li>
+		<div class="newsletter_content">
+			<h1>subscribe for a 20% discount</h1>
+			<form action="#">
+				<input type="email" class="newsletter_input" required="required" placeholder="E-mail here">
+				<button type="submit" class="button">subscribe</button>
+			</form>
+			<p class="newsletter_text">One time offer. Offer may not be combined with any other sale, promotion, discount, code, or coupon.</p>
+		</div>
+
+		<section>
+			<a href="#">Ruby's Vintage Jewelry</a>
+			<nav>
+				<ul>
+					<li><a href="#">home</a></li>
+					<li><a href="about.php">about</a></li>
+					<li><a href="https://www.etsy.com/shop/rubysvintagejewelry?ref=condensed_trust_header_title_items">store</a></li>
+					<li><a href="contact.php">contact</a></li>
+				</ul>
+			</nav>
+			<ul id="social">
+				<li><a href="https://www.facebook.com/rubylayneandtiffany/"><i class="fa fa-facebook" aria hidden+"true"></i></a></li>
+				<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
 			</ul>
-		</nav>
-		<ul id="social">
-			<li><a href="https://www.facebook.com/rubylayneandtiffany/"><i class="fa fa-facebook" aria hidden+"true"></i></a></li>
-			<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-		</ul>
+		</section>
 		<p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made by Francois Boulou</p>
-		<!-- https://www.pexels.com/photo/woman-wears-gold-colored-blue-gemstone-pendant-necklace-1035683/ -->
 	</footer>
 
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
-<script src="js/custom.js"></script>
 <script type="text/javascript" src="./js/instafeed.min.js"></script>
+<script src="js/custom.js"></script>
 </body>
 </html>
